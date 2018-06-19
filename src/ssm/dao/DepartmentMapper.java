98d98bd7a22ef023c05ffeea49f2798dcc896bc9
moranke <ldk@keranst.com>
2018-06-19 10:56:22 +0800

    qw

---
+++ src/ssm/dao/DepartmentMapper.java
@@ -0,0 +1,30 @@
+package ssm.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import ssm.entity.Department;
+import ssm.entity.DepartmentExample;
+
+public interface DepartmentMapper {
+    long countByExample(DepartmentExample example);
+
+    int deleteByExample(DepartmentExample example);
+
+    int deleteByPrimaryKey(Integer deptid);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    List<Department> selectByExample(DepartmentExample example);
+
+    Department selectByPrimaryKey(Integer deptid);
+
+    int updateByExampleSelective(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByExample(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByPrimaryKeySelective(Department record);
+
+    int updateByPrimaryKey(Department record);
+}
